faa90162dbf87c657ade6a95c9a407b773b031af
Cedar <64336732+Cedar-81@users.noreply.github.com>
2023-10-04 20:03:39 -1100

    Update +page.svelte

--- src/routes/(dashboard)/lens/+page.svelte
+++ src/routes/(dashboard)/lens/+page.svelte
@@ -24,6 +24,21 @@
 
 </script>
 
+<svelte:head>
+    <meta property="og:site_name" content="Neeva" />
+    <meta property=“og:title” content='Lens - Explore a world of unique captivating short stories'/>
+    <meta property="og:description" content="Step into the Lens, where brevity is an art form that ignites boundless creativity. In these small yet mighty stories, brevity is the canvas for boundless creativity. Dive into captivating narratives, where every word holds a universe of meaning. Join us on a journey through microfiction wonders, where every word carries the weight of a universe. Whether you're reading or crafting your own stories, Lens invites you to embrace the power of concise storytelling." />
+    <meta property="og:url" content="https://neeva.vercel.app/lens" />
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="https://images.unsplash.com/photo-1587876931567-564ce588bfbd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" />
+    <meta property="og:image:secure_url" content="https://images.unsplash.com/photo-1587876931567-564ce588bfbd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" />
+    <meta property="og:image:width" content="1280" />
+    <meta property="og:image:height" content="640" />
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:image" content="https://images.unsplash.com/photo-1587876931567-564ce588bfbd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" />
+    <meta property="twitter:site" content="@neevaverse" />
+</svelte:head>
+
 <div class="px-8 pb-10">
     <LensHeader />
     <LensList />
