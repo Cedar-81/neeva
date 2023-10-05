a9a84001ac78bf9cba861f0e88ba4ecbfd0335f3
Cedar <64336732+Cedar-81@users.noreply.github.com>
2023-10-04 20:05:00 -1100

    Update +page.svelte

--- src/routes/(dashboard)/lens/read/[id]/+page.svelte
+++ src/routes/(dashboard)/lens/read/[id]/+page.svelte
@@ -2,7 +2,12 @@
 	import { personalBio, supabaseClient } from "$lib/appStore";
 	import { lensComments, singleLens, user_id, type LensComments, lens } from "$lib/store";
 	import LensContent from "$components/lens/LensContent.svelte";
+	import { page } from "$app/stores";
 	export let data;
+
+	let { params } = $page
+    	let id = +params.id
+
 
 	singleLens.set(data.singleLens)
 	user_id.set(data.userId)
@@ -33,4 +38,15 @@
 	.subscribe()
 
 </script>
+
+<svelte:head>
+    <meta property="og:site_name" content="Neeva" />
+    <meta property=“og:title” content={`Lens - ${data.singleLens.title}`}/>
+    <meta property="og:description" content={`${data.singleLens.summary}`} />
+    <meta property="og:url" content={`https://neeva.vercel.app/lens/read/${id}`} />
+    <meta property="og:type" content="article" />
+    <meta property="og:image:width" content="1280" />
+    <meta property="og:image:height" content="640" />
+    <meta property="twitter:card" content="summary" />
+</svelte:head>
 <LensContent /> 
