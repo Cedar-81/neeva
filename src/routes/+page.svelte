0a235c2d79cf47f58c76c3f113858ad4fa82f8e7
Cedar <64336732+Cedar-81@users.noreply.github.com>
2023-10-04 20:02:44 -1100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,3 +12,18 @@
     })
 
 </script>
+
+<svelte:head>
+    <meta property="og:site_name" content="Neeva" />
+    <meta property=“og:title” content='Neeva - Crafting worlds with words'/>
+    <meta property="og:description" content="Neeva is a platform for young writers/writers to create and share their short stories, poems, flash fictions, poetry and more." />
+    <meta property="og:url" content="https://neeva.vercel.app" />
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="https://images.unsplash.com/photo-1551029506-0807df4e2031?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1634&q=80" />
+    <meta property="og:image:secure_url" content="https://images.unsplash.com/photo-1551029506-0807df4e2031?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1634&q=80" />
+    <meta property="og:image:width" content="1280" />
+    <meta property="og:image:height" content="640" />
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:image" content="https://images.unsplash.com/photo-1551029506-0807df4e2031?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1634&q=80" />
+    <meta property="twitter:site" content="@neevaverse" />
+</svelte:head>
